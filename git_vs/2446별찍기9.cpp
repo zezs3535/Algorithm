7fe222d355689caf1b4e90f57509ed996a65b800
2015041032@cbnu.ac.kr <2015041032@cbnu.ac.kr>
2019-07-06 18:09:23 +0900

    별찍기9

---
+++ git_vs/2446별찍기9.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, i, j, k;
+	cin >> n;
+	for (k = 1; k <= n; k++) { //1,2,3,4,5
+		for (j = 1; j < k; j++) {
+			printf(" ");
+		}
+		for (i = 0; i <= 2*(n-k); i++) {
+			printf("*");
+		}		
+		printf("\n");
+	}
+	for (k = n - 1; k > 0; k--) {//4,3,2,1
+		for (j = 1; j < k; j++) { 
+			printf(" ");
+		}
+		for (i = 1; i <= 2*(n-k)+1; i++) {//
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
