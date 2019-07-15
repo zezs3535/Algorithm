0fee2d31e4203c64bf353e7cab668634b682cc72
2015041032@cbnu.ac.kr <2015041032@cbnu.ac.kr>
2019-07-15 09:40:55 +0900

    11726 2xn타일링 dp문제

--- git_vs/2446별찍기9.cpp
+++ git_vs/2446별찍기9.cpp
@@ -8,25 +8,8 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n, i, j, k;
-	cin >> n;
-	for (k = 1; k <= n; k++) { //1,2,3,4,5
-		for (j = 1; j < k; j++) {
-			printf(" ");
-		}
-		for (i = 0; i <= 2*(n-k); i++) {
-			printf("*");
-		}		
-		printf("\n");
-	}
-	for (k = n - 1; k > 0; k--) {//4,3,2,1
-		for (j = 1; j < k; j++) { 
-			printf(" ");
-		}
-		for (i = 1; i <= 2*(n-k)+1; i++) {
-			printf("*");
-		}
-		printf("\n");
-	}
+	int m, n;
+	cin >> m >> n;
+	cout << m << n;
 	return 0;
 }
