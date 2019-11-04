90be09b0724948d7e644dd736c3ceb5cf988fa34
2015041032@cbnu.ac.kr <2015041032@cbnu.ac.kr>
2019-11-04 17:06:59 +0900

    dp

--- git_vs/11052번 카드 구매하기.cpp
+++ git_vs/11052번 카드 구매하기.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int card[10002];
-int dp[10002];
+int dp[10002] = { 0, };
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	int N;
+	int maxnum = 0;
 	cin >> N;
 	for (int i = 1; i <= N; i++) {
-		cin >> card[i];
-		dp[i] = card[i];
+		cin >> dp[i];
 	}
 	if (N == 1) {
-		cout << card[N];
+		cout << dp[N];
 		return 0;
 	}
 	else {
-		for (int i = 1; i <= N; i++) {
-
+		for (int i = 2; i <= N; i++) {
+			for (int j = i; j>=(i-j); j--) {
+				if (maxnum <= (dp[j] + dp[i - j]))
+					maxnum = dp[j] + dp[i - j];
+			}
+			dp[i] = max(dp[i], maxnum);
 		}
 	}
+	cout << dp[N];
 	return 0;
 }
