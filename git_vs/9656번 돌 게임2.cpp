13e025b097957558da2c7cc81ae866db3832677a
2015041032@cbnu.ac.kr <2015041032@cbnu.ac.kr>
2020-02-25 16:24:11 +0900

    알고리즘 게임

---
+++ git_vs/9656번 돌 게임2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	if (n % 2 == 0)cout << "SK";
+	else {
+		cout << "CY";
+	}
+	return 0;
+}
