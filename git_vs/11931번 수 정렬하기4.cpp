fbd4000ae32a4625bfb411d4598c3d0ec27d91cc
zezs3535 <zezs3535@naver.com>
2020-10-27 19:58:16 +0900

    정렬, 최소힙

---
+++ git_vs/11931번 수 정렬하기4.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n; cin >> n;
+	priority_queue<int, vector<int>, less<int>> pq;
+	for (int i = 0; i < n; i++) {
+		int tmp; cin >> tmp;
+		pq.push(tmp);
+	}
+	for (int i = 0; i < n; i++) {
+		cout << pq.top() << "\n";
+		pq.pop();
+	}
+	return 0;
+}
