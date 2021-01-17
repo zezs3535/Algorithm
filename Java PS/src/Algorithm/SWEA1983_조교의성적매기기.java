cf5fba3492fcbcf7138990554a3bb226d6514ae8
장찬용 <zezs3535@naver.com>
2021-01-17 19:34:50 +0900

    D2

--- Java PS/src/Algorithm/SWEA1983_조교의성적매기기.java
+++ Java PS/src/Algorithm/SWEA1983_조교의성적매기기.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class SWEA1983_조교의성적매기기 {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
