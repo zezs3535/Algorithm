74f1b16e805ec632b18db0f34106d72a1b7d828e
장찬용 <zezs3535@naver.com>
2021-01-18 16:21:27 +0900

    수학

--- Java PS/src/Algorithm/BOJ16917_양념반후라이드반.java
+++ Java PS/src/Algorithm/BOJ16917_양념반후라이드반.java
@@ -14,25 +14,22 @@ public class BOJ16917_양념반후라이드반 {
 		int x = sc.nextInt();
 		int y = sc.nextInt();
 		int price = 0;
-		int small=Math.min(x, y);
-		
+		int small = Math.min(x, y);
+
 		if (a + b >= (2 * c)) {
-			price = small*c*2;
-			if(x-small>0 && a>=c*2) {
-				price+=(x-small)*2*c;
+			price = small * c * 2;
+			if (x - small > 0 && a >= c * 2) {
+				price += (x - small) * 2 * c;
+			} else if (x - small > 0 && a < c * 2) {
+				price += (x - small) * a;
 			}
-			else if(x-small>0 && a<c*2) {
-				price+=(x-small)*a;
+			if (y - small > 0 && b >= c * 2) {
+				price += (y - small) * 2 * c;
+			} else if (y - small > 0 && b < c * 2) {
+				price += (y - small) * b;
 			}
-			if(y-small>0 && b>=c*2) {
-				price+=(y-small)*2*c;
-			}
-			else if(y-small>0 && b<c*2) {
-				price+=(y-small)*b;
-			}
-		}
-		else {
-			price = a*x+b*y;
+		} else {
+			price = a * x + b * y;
 		}
 		System.out.println(price);
 	}
