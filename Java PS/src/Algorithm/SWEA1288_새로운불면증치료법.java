a24fc2f286a549ec59349b56e42e4af524c681c2
장찬용 <zezs3535@naver.com>
2021-01-18 16:04:20 +0900

    D2,비트마스킹

--- Java PS/src/Algorithm/SWEA1288_새로운불면증치료법.java
+++ Java PS/src/Algorithm/SWEA1288_새로운불면증치료법.java
@@ -6,6 +6,7 @@ public class SWEA1288_새로운불면증치료법 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
 		for (int i = 0; i < t; i++) {
