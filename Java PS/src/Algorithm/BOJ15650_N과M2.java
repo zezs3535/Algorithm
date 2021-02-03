72e7bf6299587e219b397799122dabd49fcd5dab
장찬용 <zezs3535@naver.com>
2021-02-03 14:06:36 +0900

    재귀, 백트래킹, 오름차순

---
+++ Java PS/src/Algorithm/BOJ15650_N과M2.java
@@ -0,0 +1,45 @@
+package Algorithm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ15650_N과M2 {
+	static int n;
+	static int m;
+	static int[] num; // 1, 2, 3.... 숫자
+	static int[] arr; // 값을 담아서 출력할 배열
+	static boolean[] v; // 방문했냐?
+
+	private static void permu(int k) {
+		if (k == m) {
+			for (int i : arr) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = 0; i < n; i++) {
+			if (!v[i]) {
+				if(k!=0 && arr[k-1] > num[i])continue;
+				v[i] = true;
+				arr[k] = num[i];
+				permu(k + 1);
+				v[i] = false;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		num = new int[n];
+		arr = new int[m];
+		v = new boolean[n];
+		for (int i = 0; i < n; i++) {
+			num[i] = i + 1;
+		}
+		permu(0);
+	}
+
+}
