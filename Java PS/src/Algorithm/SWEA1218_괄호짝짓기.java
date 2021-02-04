d15f1169600d4cd5a07da0b0027c7592dba5af00
장찬용 <zezs3535@naver.com>
2021-02-04 13:25:59 +0900

    D4, 스택

---
+++ Java PS/src/Algorithm/SWEA1218_괄호짝짓기.java
@@ -0,0 +1,71 @@
+package Algorithm;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class SWEA1218_괄호짝짓기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for (int t = 1; t <= 10; t++) {
+			boolean isValid = true;
+			int n = sc.nextInt();
+			Stack<Character> st = new Stack<Character>();
+			String s = sc.next();
+			for (int i = 0; i < s.length(); i++) {
+				if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['||s.charAt(i)=='<') {
+					st.push(s.charAt(i));
+					continue;
+				}
+				if(s.charAt(i)==')') {
+					if(st.peek()=='(') {
+						st.pop();
+						continue;
+					}
+					else {
+						isValid=false;
+						break;
+					}
+				}
+				if(s.charAt(i)=='}') {
+					if(st.peek()=='{') {
+						st.pop();
+						continue;
+					}
+					else {
+						isValid=false;
+						break;
+					}
+				}
+				if(s.charAt(i)==']') {
+					if(st.peek()=='[') {
+						st.pop();
+						continue;
+					}
+					else {
+						isValid=false;
+						break;
+					}
+				}
+				if(s.charAt(i)=='>') {
+					if(st.peek()=='<') {
+						st.pop();
+						continue;
+					}
+					else {
+						isValid=false;
+						break;
+					}
+				}
+			}
+			if(!isValid ||!st.empty()) {
+				System.out.println("#"+t+" "+0);
+			}
+			else {
+				System.out.println("#"+t+" "+1);
+			}
+		}
+
+	}
+
+}
