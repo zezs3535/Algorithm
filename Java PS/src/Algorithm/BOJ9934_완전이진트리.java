4d1c3670760f30900e7685d60ec0714653cff0d0
장찬용 <zezs3535@naver.com>
2021-02-17 11:44:13 +0900

    구현

---
+++ Java PS/src/Algorithm/BOJ9934_완전이진트리.java
@@ -0,0 +1,26 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BOJ9934_완전이진트리 {
+	
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
