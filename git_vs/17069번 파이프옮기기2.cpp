a198501eafd4647390574f9313a8e08b04e48cd3
장찬용 <zezs3535@naver.com>
2021-03-10 17:47:59 +0900

    DP

---
+++ git_vs/17069번 파이프옮기기2.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+int n;
+struct Pipe {
+	long long dir[4]; //1,2,3 순서로 3시/5시/6시
+};
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n;
+	long long answer = 0;
+	vector<vector<Pipe>> dp(n, vector<Pipe>(n)); //dp배열
+	vector<vector<int>> arr(n, vector<int>(n, 0)); //지도
+	dp[0][1].dir[1] = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) { //배열 순회
+			int ny = i + 1;
+			int nx = j + 1;
+			if (dp[i][j].dir[1] != 0) { //파이프가 오른쪽으로 나있을때
+				if (nx < n && arr[i][nx] == 0)dp[i][nx].dir[1] += dp[i][j].dir[1]; //오른쪽으로 연결
+				if (nx < n && ny < n && arr[ny][nx] == 0 && arr[ny][j] == 0 && arr[i][nx] == 0)dp[ny][nx].dir[2] += dp[i][j].dir[1]; //오른쪽아래로 연결
+			}
+			if (dp[i][j].dir[2] != 0) { //파이프가 오른쪽아래로 나있을때
+				if (nx < n && arr[i][nx] == 0)dp[i][nx].dir[1] += dp[i][j].dir[2]; //오른쪽으로 연결
+				if (nx < n && ny < n && arr[ny][nx] == 0 && arr[ny][j] == 0 && arr[i][nx] == 0)dp[ny][nx].dir[2] += dp[i][j].dir[2]; //오른쪽아래로 연결
+				if (ny < n && arr[ny][j] == 0)dp[ny][j].dir[3] += dp[i][j].dir[2];
+			}
+			if (dp[i][j].dir[3] != 0) { //파이프가 아래로 났을때
+				if (nx < n && ny < n && arr[ny][nx] == 0 && arr[ny][j] == 0 && arr[i][nx] == 0)dp[ny][nx].dir[2] += dp[i][j].dir[3]; //오른쪽아래로 연결
+				if (ny < n && arr[ny][j] == 0)dp[ny][j].dir[3] += dp[i][j].dir[3];
+			}
+		}
+	}
+	for (int i = 1; i <= 3; i++) {
+		answer += dp[n - 1][n - 1].dir[i];
+	}
+	cout << answer;
+	return 0;
+}
