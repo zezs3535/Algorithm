2cd27c6ce8f67aa11698d25bed8b351b7d787caa
장찬용 <zezs3535@naver.com>
2021-04-07 21:08:17 +0900

    DP

--- git_vs/17271번 리그오브레전설small.cpp
+++ git_vs/17271번 리그오브레전설small.cpp
@@ -2,21 +2,28 @@
 #define MOD 1000000007
 using namespace std;
 
-typedef pair<int, int> pii;
-
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	int n, m;
-	int dp[10001] = { 0, };
-	cin >> n >> m;
-	for (int i = 0; i <= m; i++) {
-		dp[i] = 1;
-	}
-	for (int i = m; i <= n; i++) {
-		dp[i] = (dp[i - 1]%MOD + dp[i - m]%MOD)%MOD;
-	}
-	cout << dp[n];
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+
+    //DP[i] = i초일때 사용할 수 있는 스킬 조합의 수
+    //값을 저장할 DP배열
+    int dp[10001] = { 0, };
+
+    cin >> n >> m;
+
+    for (int i = 0; i <= m; i++) { //DP배열을 다 1로 초기화
+        dp[i] = 1;
+    }
+
+    for (int i = m; i <= n; i++) { //i초일때는 i-1초에서 1초 걸리는 A스킬을 더 쓰거나
+    //i-m초에서 m초 걸리는 B스킬을 더 쓰는 경우 두 가지 경우를 더한 값
+        dp[i] = (dp[i - 1] % MOD + dp[i - m] % MOD) % MOD;
+    }
+
+    cout << dp[n];
+    return 0;
 }
