84ed48710ebf6c6f4c293cb010f24643b33ae0f5
장찬용 <zezs3535@naver.com>
2021-04-19 13:33:01 +0900

    수학

--- Java PS/src/Algorithm/SWEA8382_방향전환.java
+++ Java PS/src/Algorithm/SWEA8382_방향전환.java
@@ -4,8 +4,8 @@ import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class SWEA8382_방향전환 {
-	
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("input.txt"));
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
@@ -14,10 +14,11 @@ public class SWEA8382_방향전환 {
 			int y1 = sc.nextInt();
 			int x2 = sc.nextInt();
 			int y2 = sc.nextInt();
-			int ysum = Math.abs(y2-y1);
-			int xsum = Math.abs(x2-x1);
-			int sum = ysum+xsum;
-			System.out.println("#"+test+" "+(sum%2==0?Math.max(ysum, xsum)*2:Math.max(ysum, xsum)*2-1));
+			int ysum = Math.abs(y2 - y1);
+			int xsum = Math.abs(x2 - x1);
+			int sum = ysum + xsum;
+			System.out.println(
+					"#" + test + " " + (sum % 2 == 0 ? Math.max(ysum, xsum) * 2 : Math.max(ysum, xsum) * 2 - 1));
 		}
 	}
 }
