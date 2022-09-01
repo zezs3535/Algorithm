80a6e314166b29a203d963be866794feb7c9f5ec
zezs3535 <44562780+zezs3535@users.noreply.github.com>
2022-09-01 19:35:49 +0900

    Time: 496 ms (5.18%), Space: 69.7 MB (7.56%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -3,10 +3,6 @@ class Solution {
         int[] arr = new int[n+1]; // 소수 0 아니면 1
         int num = (int)Math.sqrt(n);
         int ans = 0;
-        if(n>=2){
-            arr[0] = 1;
-            arr[1] = 1;
-        }
         for(int i=2;i<=num;i++){
             for(int j=2;j*i<=n;j++){
                 arr[i*j] = 1;
