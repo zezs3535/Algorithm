dfede41bee8a2e633dffaf6a407cc1c16f1bd7ca
zezs3535 <44562780+zezs3535@users.noreply.github.com>
2022-09-01 19:37:30 +0900

    Time: 278 ms (18.15%), Space: 46.7 MB (94.23%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -1,15 +1,15 @@
 class Solution {
     public int countPrimes(int n) {
-        int[] arr = new int[n+1]; // 소수 0 아니면 1
+        boolean[] arr = new boolean[n+1]; // 소수 0 아니면 1
         int num = (int)Math.sqrt(n);
         int ans = 0;
         for(int i=2;i<=num;i++){
             for(int j=2;j*i<=n;j++){
-                arr[i*j] = 1;
+                arr[i*j] = true;
             }
         }
         for(int i=2;i<n;i++){
-            if(arr[i]==0)ans++;
+            if(arr[i]==false)ans++;
         }
         return ans;
     }
