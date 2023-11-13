b1f1d35faf3bbf04b20b9d924966e8dc95f66b85
zezs3535 <44562780+zezs3535@users.noreply.github.com>
2023-11-13 16:54:50 +0900

    Time: 43 ms (72.48%), Space: 50.1 MB (8.29%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
@@ -2,26 +2,16 @@ import java.util.*;
 
 class Solution {
     
-    public static class Ja{
-        char c;
-        int idx;
-        Ja(char c, int idx){
-            this.c = c;
-            this.idx = idx;
-        }
-    }
-    
     public String sortVowels(String s) {
         int n = s.length();
-        ArrayList<Ja> jal = new ArrayList<>();
+        ArrayList<Integer> jal = new ArrayList<>();
         ArrayList<Character> mol = new ArrayList<>();
         
         for(int i=0;i<n;i++){
             int cur = s.charAt(i)-'A';
             if(cur==0||cur==4||cur==8||cur==14||cur==20||cur==32||cur==36||cur==40||cur==46||cur==52){
                 mol.add(s.charAt(i));
-            }else{
-                jal.add(new Ja(s.charAt(i),i));
+                jal.add(i);
             }
         }
         
@@ -29,17 +19,9 @@ class Solution {
         
         int jaIdx=0;
         int moIdx=0;
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<n;i++){
-            if(jal.size() != 0 && jaIdx < jal.size() && jal.get(jaIdx).idx==i){
-                sb.append(jal.get(jaIdx).c);
-                jaIdx++;
-            }else{
-                sb.append(mol.get(moIdx));
-                moIdx++;
-            }
-        }
-        
-        return sb.toString();
+        char[] ans = s.toCharArray();
+        for(int i=0;i<jal.size();i++)ans[jal.get(i)]=mol.get(i);
+            
+        return new String(ans);
     }
 }
